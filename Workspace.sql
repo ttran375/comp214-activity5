2fcc723323b875ba9228bb0e9387fd33f1004c36
T. Tran <ttran375@my.centennialcollege.ca>
2024-03-28 22:42:59 +0000

    Thu, Mar 28, 2024, 10:42 PM +00:00

--- Workspace.sql
+++ Workspace.sql
@@ -40,6 +40,45 @@ WHERE
 -- SELECT object_name, status
 -- FROM user_objects
 -- WHERE object_name IN ('STATUS_CHECK_SP','STATUS_DESC_SF');
+
+CREATE OR REPLACE FUNCTION status_desc_sf
+  (p_stage IN NUMBER)
+  RETURN VARCHAR2
+ IS
+  lv_stage_txt VARCHAR2(30);
+BEGIN
+ IF p_stage = 1 THEN
+   lv_stage_txt := 'Order Submitted';
+ ELSIF p_stage = 2 THEN
+   lv_stage_txt := 'Accepted, sent to shipping';
+ ELSIF p_stage = 3 THEN
+   lv_stage_txt := 'Backordered';
+ ELSIF p_stage = 4 THEN
+   lv_stage_txt := 'Cancelled';
+ ELSIF p_stage = 5 THEN
+   lv_stage_txt := 'Shipped';
+ END IF;
+ RETURN lv_stage_txt;
+END;
+
+CREATE OR REPLACE PROCEDURE status_check_sp
+  (p_bask IN NUMBER,
+   p_stage OUT NUMBER,
+   p_desc OUT VARCHAR2)
+ IS
+BEGIN
+  SELECT idstage
+   INTO p_stage
+   FROM bb_basketstatus
+   WHERE idBasket = p_bask;
+  p_desc := status_desc_sf(p_stage);
+END;
+/
+
+SELECT object_name, status
+FROM user_objects
+WHERE object_name IN ('STATUS_CHECK_SP','STATUS_DESC_SF');
+
 -- 3. The STATUS_DESC_SF function adds a description of the numeric value for the IDSTAGE column. The company needs to add another order status stage for situations in which credit card approval fails. In SQL Developer, modify the function by adding the following ELSIF clause, and compile it. (Don’t compile or run the function again.)
 -- ELSIF p_stage = 6 THEN
 -- lv_stage_txt := 'Credit Card Not Approved';
