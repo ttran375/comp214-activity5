23086ab376e7eb15c59d7683141c89d7acedd7f1
T. Tran <ttran375@my.centennialcollege.ca>
2024-03-28 22:45:33 +0000

    Thu, Mar 28, 2024, 10:45 PM +00:00

--- Workspace.sql
+++ Workspace.sql
@@ -60,6 +60,7 @@ BEGIN
  END IF;
  RETURN lv_stage_txt;
 END;
+/
 
 CREATE OR REPLACE PROCEDURE status_check_sp
   (p_bask IN NUMBER,
@@ -82,6 +83,29 @@ WHERE object_name IN ('STATUS_CHECK_SP','STATUS_DESC_SF');
 -- 3. The STATUS_DESC_SF function adds a description of the numeric value for the IDSTAGE column. The company needs to add another order status stage for situations in which credit card approval fails. In SQL Developer, modify the function by adding the following ELSIF clause, and compile it. (Don’t compile or run the function again.)
 -- ELSIF p_stage = 6 THEN
 -- lv_stage_txt := 'Credit Card Not Approved';
+CREATE OR REPLACE FUNCTION status_desc_sf
+  (p_stage IN NUMBER)
+  RETURN VARCHAR2
+ IS
+  lv_stage_txt VARCHAR2(30);
+BEGIN
+ IF p_stage = 1 THEN
+   lv_stage_txt := 'Order Submitted';
+ ELSIF p_stage = 2 THEN
+   lv_stage_txt := 'Accepted, sent to shipping';
+ ELSIF p_stage = 3 THEN
+   lv_stage_txt := 'Backordered';
+ ELSIF p_stage = 4 THEN
+   lv_stage_txt := 'Cancelled';
+ ELSIF p_stage = 5 THEN
+   lv_stage_txt := 'Shipped';
+ ELSIF p_stage = 6 THEN -- Added this clause to handle credit card not approved status
+   lv_stage_txt := 'Credit Card Not Approved';
+ END IF;
+ RETURN lv_stage_txt;
+END;
+/
+
 -- 4. Does the modification in Step 3 affect the STATUS_CHECK_SP procedure’s status? Verify by repeating the query in Step 2. The procedure is dependent on the function, so it’s now INVALID and must be recompiled.
 -- 5. Call the procedure for basket 13, as shown in the following code:
 -- DECLARE
