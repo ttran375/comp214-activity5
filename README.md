31983b3077702300531871d3e23294a3eb771198
T. Tran <ttran375@my.centennialcollege.ca>
2024-03-28 22:27:56 +0000

    Update solutions

--- README.md
+++ README.md
@@ -2,57 +2,31 @@
 
 ## Question 1
 
-Assignment 8-1: Reviewing Dependency Informa;on in the Data Dic;onary
-Two data dic/onary views store informa/on on dependencies: USER_OBJECTS and
-USER_DEPENDENCIES. Take a closer look at these views to examine the informa/on in them:
-1. In SQL Developer, issue a DESCRIBE command on the USER_OBJECTS view and review the
-available columns. Which columns are par/cularly relevant to dependencies? The STATUS
-column indicates whether the object is VALID or INVALID. The TIMESTAMP column is used in
-remote connec/ons to determine invalida/on.
-2. Query the USER_OBJECTS view, selec/ng the OBJECT_NAME, STATUS, and TIMESTAMP columns
-for all procedures. Recall that you can use a WHERE clause to look for object types of
-PROCEDURE to list only procedure informa/on.
-3. Now issue a DESCRIBE command on the USER_DEPENDENCIES view to review the available
-columns. If you query this table for the name of a specific object, a list of all the objects it
-references is displayed. However, if you query for a specific referenced name, you see a list of
-objects that are dependent on this par/cular object.
-4. Say you intend to make a modifica/on to the BB_BASKET table and need to iden/fy all
-dependent program units to finish recompiling. Run the following query to list all objects that
-are dependent on the BB_BASKET table:
- SELECT name, type
+Assignment 8-1: Reviewing Dependency Information in the Data Dictionary
+Two data dictionary views store information on dependencies: USER_OBJECTS and USER_DEPENDENCIES. Take a closer look at these views to examine the information in them:
+1. In SQL Developer, issue a DESCRIBE command on the USER_OBJECTS view and review the available columns. Which columns are particularly relevant to dependencies? The STATUS column indicates whether the object is VALID or INVALID. The TIMESTAMP column is used in remote connections to determine invalidation.
+2. Query the USER_OBJECTS view, selecting the OBJECT_NAME, STATUS, and TIMESTAMP columns for all procedures. Recall that you can use a WHERE clause to look for object types of PROCEDURE to list only procedure information.
+3. Now issue a DESCRIBE command on the USER_DEPENDENCIES view to review the available columns. If you query this table for the name of a specific object, a list of all the objects it references is displayed. However, if you query for a specific referenced name, you see a list of objects that are dependent on this particular object.
+4. Say you intend to make a modification to the BB_BASKET table and need to identify all dependent program units to finish recompiling. Run the following query to list all objects that are dependent on the BB_BASKET table:
+SELECT name, type
 FROM user_dependencies
 WHERE referenced_name = 'BB_BASKET';
 
 ## Question 2
 
-Assignment 8-2: Tes;ng Dependencies on Stand-Alone Program Units
-In this assignment, you verify the effect of object modifica/ons on the status of dependent objects. You
-work with a procedure and a func/on.
-1. In a text editor, open the assignment08-02.txt file in the Chapter08 folder. This file contains
-statements to create the STATUS_CHECK_SP procedure and the STATUS_DESC_SF func/on.
-Review the code, and no/ce that the procedure includes a call to the func/on. Use the code in
-this file to create the two program units in SQL Developer.
+Assignment 8-2: Testing Dependencies on Stand-Alone Program Units
+In this assignment, you verify the effect of object modifications on the status of dependent objects. You work with a procedure and a function.
+1. In a text editor, open the assignment08-02.txt file in the Chapter08 folder. This file contains statements to create the STATUS_CHECK_SP procedure and the STATUS_DESC_SF function. Review the code, and notice that the procedure includes a call to the function. Use the code in this file to create the two program units in SQL Developer.
 2. Enter and run the following query to verify that the status of both objects is VALID:
- SELECT object_name, status
+SELECT object_name, status
 FROM user_objects
-WHERE object_name IN
-('STATUS_CHECK_SP','STATUS_DESC_SF');
-3. The STATUS_DESC_SF func/on adds a descrip/on of the numeric value for the IDSTAGE column.
-The company needs to add another order status stage for situa/ons in which credit card
-approval fails. In SQL Developer, modify the func/on by adding the following ELSIF clause, and
-compile it. (Don’t compile or run the func/on again.)
+WHERE object_name IN ('STATUS_CHECK_SP','STATUS_DESC_SF');
+3. The STATUS_DESC_SF function adds a description of the numeric value for the IDSTAGE column. The company needs to add another order status stage for situations in which credit card approval fails. In SQL Developer, modify the function by adding the following ELSIF clause, and compile it. (Don’t compile or run the function again.)
 ELSIF p_stage = 6 THEN
 lv_stage_txt := 'Credit Card Not Approved';
-4. Does the modifica/on in Step 3 affect the STATUS_CHECK_SP procedure’s status? Verify by
-repea/ng the query in Step 2. The procedure is dependent on the func/on, so it’s now INVALID
-and must be recompiled.
+4. Does the modification in Step 3 affect the STATUS_CHECK_SP procedure’s status? Verify by repeating the query in Step 2. The procedure is dependent on the function, so it’s now INVALID and must be recompiled.
 5. Call the procedure for basket 13, as shown in the following code:
- DECLARE
-lv_stage_num NUMBER(2);
-lv_desc_txt VARCHAR2(30);
- BEGIN
-status_check_sp(13,lv_stage_num,lv_desc_txt);
- END;
-6. Repeat the query in Step 2 to verify the STATUS_CHECK_SP procedure’s status. The procedure
-now shows the status VALID as a result of automa/c recompiling when the procedure was
-called.
+DECLARE
+lv_stage_num NUMBER(2); lv_desc_txt VARCHAR2(30);
+BEGIN status_check_sp(13,lv_stage_num,lv_desc_txt); END;
+6. Repeat the query in Step 2 to verify the STATUS_CHECK_SP procedure’s status. The procedure now shows the status VALID as a result of automatic recompiling when the procedure was called.
--- Workspace.sql
+++ Workspace.sql
@@ -1,190 +1,41 @@
--- 1. Assignment 7-2 from text: Using Program Units in a Package
--- In this activity, you use program units in a package created to store basket information. The
--- package contains a function that returns the recipient’s name and a procedure that retrieves the
--- shopper ID and order date for a basket.
--- a. In SQL Developer, create the ORDER_INFO_PKG package, using the Assignment07-
--- 02.txt file located in eCentennial. Review the code to become familiar with the two
--- program units in the package.
--- b. Create an anonymous block that calls both the packaged procedure and function with
--- basket ID 12 to test these program units. Use DBMS_OUTPUT statements to display
--- values returned from the program units to verify the data.
--- c. Also, test the packaged function by using it in a SELECT clause on the BB_BASKET
--- table.
--- d. Use a WHERE clause to select only the basket 12 row.
-CREATE OR REPLACE PACKAGE order_info_pkg IS
+-- ## Question 1
 
-    FUNCTION ship_name_pf (
-        p_basket IN NUMBER
-    ) RETURN VARCHAR2;
+-- Assignment 8-1: Reviewing Dependency Information in the Data Dictionary
+-- Two data dictionary views store information on dependencies: USER_OBJECTS and USER_DEPENDENCIES. Take a closer look at these views to examine the information in them:
+-- 1. In SQL Developer, issue a DESCRIBE command on the USER_OBJECTS view and review the available columns. Which columns are particularly relevant to dependencies? The STATUS column indicates whether the object is VALID or INVALID. The TIMESTAMP column is used in remote connections to determine invalidation.
+-- 2. Query the USER_OBJECTS view, selecting the OBJECT_NAME, STATUS, and TIMESTAMP columns for all procedures. Recall that you can use a WHERE clause to look for object types of PROCEDURE to list only procedure information.
+-- 3. Now issue a DESCRIBE command on the USER_DEPENDENCIES view to review the available columns. If you query this table for the name of a specific object, a list of all the objects it references is displayed. However, if you query for a specific referenced name, you see a list of objects that are dependent on this particular object.
+-- 4. Say you intend to make a modification to the BB_BASKET table and need to identify all dependent program units to finish recompiling. Run the following query to list all objects that are dependent on the BB_BASKET table:
+-- SELECT name, type
+-- FROM user_dependencies
+-- WHERE referenced_name = 'BB_BASKET';
+desc user_objects;
 
-    PROCEDURE basket_info_pp (
-        p_basket IN NUMBER,
-        p_shop OUT NUMBER,
-        p_date OUT DATE
-    );
-END;
 /
 
-CREATE OR REPLACE PACKAGE BODY order_info_pkg IS
-
-    FUNCTION ship_name_pf (
-        p_basket IN NUMBER
-    ) RETURN VARCHAR2 IS
-        lv_name_txt VARCHAR2(25);
-    BEGIN
-        SELECT
-            shipfirstname
-            ||' '
-            ||shiplastname INTO lv_name_txt
-        FROM
-            bb_basket
-        WHERE
-            idBasket = p_basket;
-        RETURN lv_name_txt;
-    EXCEPTION
-        WHEN NO_DATA_FOUND THEN
-            DBMS_OUTPUT.PUT_LINE('Invalid basket id');
-    END ship_name_pf;
-
-    PROCEDURE basket_info_pp (
-        p_basket IN NUMBER,
-        p_shop OUT NUMBER,
-        p_date OUT DATE
-    ) IS
-    BEGIN
-        SELECT
-            idshopper,
-            dtordered INTO p_shop,
-            p_date
-        FROM
-            bb_basket
-        WHERE
-            idbasket = p_basket;
-    EXCEPTION
-        WHEN NO_DATA_FOUND THEN
-            DBMS_OUTPUT.PUT_LINE('Invalid basket id');
-    END basket_info_pp;
-END;
-/
-
--- test procedure and function in block (returns nulls)
-DECLARE
-    lv_id      NUMBER := 12;
-    lv_name    VARCHAR2(25);
-    lv_shopper bb_basket.idshopper%type;
-    lv_date    bb_basket.dtcreated%type;
-BEGIN
- -- test function
-    lv_name := order_info_pkg.ship_name_pf(lv_id);
-    dbms_output.put_line(lv_id
-                         ||' '
-                         ||lv_name);
- -- test procedure
-    order_info_pkg.basket_info_pp(lv_id, lv_shopper, lv_date);
-    dbms_output.put_line(lv_id
-                         ||' '
-                         ||lv_shopper
-                         ||' '
-                         ||lv_date);
-END;
-/
-
--- test with select - again, prints nothing
 SELECT
-    lpad(order_info_pkg.ship_name_pf(idbasket), 20) "ship_name_pf on 12"
+  rpad(object_name, 23) "OBJECT_NAME",
+  status,
+  timestamp
 FROM
-    bb_basket
+  user_objects
 WHERE
-    idbasket = 12;
+  object_type = 'PROCEDURE';
 
 /
 
--- 2. Assignment 7-3 from text: Creating a Package with Private Program Units
--- In this activity, you modify a package to make program units private. The Brewbean’s
--- programming group decided that the SHIP_NAME_PF function in the ORDER_INFO_PKG
--- package should be used only from inside the package. Follow these steps to make this
--- modification:
--- a. In Notepad, open the Assignment07-03.txt file in the Chapter07 folder, and review the
--- package code.
--- b. Modify the package code to add to the BASKET_INFO_PP procedure so that it also
--- returns the name an order is shipped by using the SHIP_NAME_PF function. Make the
--- necessary changes to make the SHIP_NAME_PF function private.
--- c. Create the package by using the modified code.
--- d. Create and run an anonymous block that calls the BASKET_INFO_PP procedure and
--- displays the shopper ID, order date, and shipped-to name to check the values returned.
--- e. Use DBMS_OUTPUT statements to display the values.
-CREATE OR REPLACE PACKAGE order_info_pkg IS
- -- deleted function
-    PROCEDURE basket_info_pp (
-        p_basket IN NUMBER,
-        p_shop OUT NUMBER,
-        p_date OUT DATE,
-        p_ship OUT VARCHAR
-    ); -- added out variable
-END;
+desc user_dependencies;
+
 /
 
-CREATE OR REPLACE PACKAGE BODY order_info_pkg IS
+column name format a15
 
-    FUNCTION ship_name_pf (
-        p_basket IN NUMBER
-    ) RETURN VARCHAR2 IS
-        lv_name_txt VARCHAR2(25);
-    BEGIN
-        SELECT
-            shipfirstname
-            ||' '
-            ||shiplastname INTO lv_name_txt
-        FROM
-            bb_basket
-        WHERE
-            idBasket = p_basket;
-        RETURN lv_name_txt;
-    EXCEPTION
-        WHEN NO_DATA_FOUND THEN
-            DBMS_OUTPUT.PUT_LINE('Invalid basket id');
-    END ship_name_pf;
+SELECT
+  name,
+  type
+FROM
+  user_dependencies
+WHERE
+  referenced_name = 'BB_BASKET';
 
-    PROCEDURE basket_info_pp (
-        p_basket IN NUMBER,
-        p_shop OUT NUMBER,
-        p_date OUT DATE,
-        p_ship OUT VARCHAR
-    ) -- added out variable
-    IS
-    BEGIN
-        SELECT
-            idshopper,
-            dtordered INTO p_shop,
-            p_date
-        FROM
-            bb_basket
-        WHERE
-            idbasket = p_basket;
-        p_ship := ship_name_pf(p_basket); -- out variable used here
-    EXCEPTION
-        WHEN NO_DATA_FOUND THEN
-            DBMS_OUTPUT.PUT_LINE('Invalid basket id');
-    END basket_info_pp;
-END;
-/
-
--- test procedure in block, this time I used basket
--- id 6, so we could actually see a name
-DECLARE
-    lv_id      NUMBER := 6;
-    lv_name    VARCHAR2(25);
-    lv_shopper bb_basket.idshopper%type;
-    lv_date    bb_basket.dtcreated%type;
-BEGIN
- -- test procedure
-    order_info_pkg.basket_info_pp(lv_id, lv_shopper, lv_date, lv_name);
-    dbms_output.put_line(lv_id
-                         ||' '
-                         ||lv_shopper
-                         ||' '
-                         ||lv_date
-                         ||' '
-                         ||lv_name);
-END;
 /
